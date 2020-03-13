a2a35683ed1c2bd3512dfa8d9fe036533e87db2b
Jared Wu <jhengsyuan626@gmail.com>
2020-03-13 22:33:10 +0800

    Initial Commit

---
+++ PeakMall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeakMall
+//
+//  Created by Jared Wu on 2020/3/13.
+//  Copyright © 2020 Jared Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
